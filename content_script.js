c7fd1685b75cda50a92a46f1296e34dbda5f99f7
Chris Herdt <cherdt@law.upenn.edu>
2016-12-03 20:21:02 -0500

    Place functions inside container variable

--- content_script.js
+++ content_script.js
@@ -1,16 +1,19 @@
-var convertAllImages = function (node) {
-    'use strict';
-    var i, images = node.getElementsByTagName("img");
-    console.log(images.length);
-    for (i = 0; i < images.length; i = i + 1) {
-        images[i].src = "http://osric.com/chris/images/business-cat-meme.jpg";
+var THEYLIVE = {
+    convertAllImages: function (node) {
+        'use strict';
+        var i, images = node.getElementsByTagName("img");
+        console.log(images.length);
+        for (i = 0; i < images.length; i = i + 1) {
+            images[i].src = "http://osric.com/chris/images/business-cat-meme.jpg";
+        }
+    },
+
+    scrollFunction: function () {
+        'use strict';
+        this.convertAllImages(document.body);
     }
 };
 
-convertAllImages(document.body);
-function scrollFunction() {
-    'use strict';
-    convertAllImages(document.body);
-}
 
-window.onscroll = scrollFunction;
+THEYLIVE.convertAllImages(document.body);
+window.onscroll = THEYLIVE.scrollFunction;
