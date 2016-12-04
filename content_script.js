5f577a2b12380353dd17bb5dcf0496152fb4375e
Chris Herdt <cherdt@law.upenn.edu>
2016-12-03 21:01:33 -0500

    Fix scroll error

--- content_script.js
+++ content_script.js
@@ -1,19 +1,21 @@
 var THEYLIVE = {
+
     convertAllImages: function (node) {
         'use strict';
-        var i, images = node.getElementsByTagName("img");
+        var i, images;
+        if (node instanceof Event) {
+            console.log("It's an event");
+            node = document.body;
+        }
+        images = node.getElementsByTagName("img");
         console.log(images.length);
         for (i = 0; i < images.length; i = i + 1) {
             images[i].src = "http://osric.com/chris/images/business-cat-meme.jpg";
         }
-    },
-
-    scrollFunction: function () {
-        'use strict';
-        this.convertAllImages(document.body);
     }
+
 };
 
 
 THEYLIVE.convertAllImages(document.body);
-window.onscroll = THEYLIVE.scrollFunction;
+window.onscroll = THEYLIVE.convertAllImages;
